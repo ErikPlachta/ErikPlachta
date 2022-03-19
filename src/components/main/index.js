83cbf334ca58b38f9f37ccbe2a1a8c2825dbdfeb
Erik Plachta <eplachta@gmail.com>
2022-03-19 19:39:52 -0400

    Add styling to push footer to bottom out of main so I can focus on it

--- src/components/main/index.js
+++ src/components/main/index.js
@@ -1,15 +1,25 @@
-import React from 'react'
+import React, {useState} from 'react'
 import AboutMe from './about-me';
 import Portfolio from './portfolio';
 import Resume from './resume';
 import Contact from './contact';
 
-export default function Main(props) {
+export default function Main({ locations, locationCurrent, setLocation}) {
+
+    const [locationContent] = useState([
+        {name: 'about', import: <AboutMe></AboutMe>},
+        {name: 'portfolio', import: <Portfolio></Portfolio>},
+        {name: 'resume', import: <Resume></Resume>},
+        {name: 'contact', import: <Contact></Contact>},
+      ])
+
     return ([
-        <h2>Main Section</h2>,
-        <AboutMe></AboutMe>,
-        <Portfolio></Portfolio>,
-        <Resume></Resume>,
-        <Contact></Contact>,
-    ]);
+        <section>
+            {locations.map( (location) => (
+                // console.log(location)
+                <span>{location.name}</span>
+            ))}
+
+        </section>
+]);
 };
