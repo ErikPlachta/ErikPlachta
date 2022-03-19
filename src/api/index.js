e02c3a970f48d999f509d1ede82b281be0d2b308
Erik Plachta <eplachta@gmail.com>
2022-03-19 13:55:04 -0400

    Onboarded base content into index.js and cleaned up app.js

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './assets/css/styles.css';
-import './assets/js/script.js';
+import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
@@ -12,5 +11,7 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// Having web-vitals print for awarness.  ( SRC -> https://bit.ly/CRA-vitals )
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
