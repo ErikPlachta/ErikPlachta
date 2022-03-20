12aff8f8d8cca3524456aee4863935dc10fbd1e9
Erik Plachta <eplachta@gmail.com>
2022-03-19 21:02:05 -0400

    .

--- src/components/main/about-me/index.js
+++ src/components/main/about-me/index.js
@@ -5,7 +5,7 @@ export default function AboutMe() {
     <section className='container'>
       <h2>About Me</h2>
       <div className="content">
-        Duis minim ut mollit Lorem nisi voluptate velit. Eu tempor non tempor eiusmod non magna aute non qui id ex labore tempor. Ad dolore esse deserunt esse eu labore minim. Elit aute pariatur consequat eu occaecat ea voluptate exercitation non. Dolor magna qui eu commodo anim labore cillum adipisicing Lorem nisi ipsum ad.
+        About me Content goes here
       </div>
     </section>
   )
