c421816da21d52ad1a89ca27a5f89bea999736f1
Erik Plachta <eplachta@gmail.com>
2022-03-19 20:18:23 -0400

    Add placerholder styling for all main sub-components and verified integrity mobile vs desktop

--- src/components/main/contact/index.js
+++ src/components/main/contact/index.js
@@ -2,6 +2,11 @@ import React from 'react'
 
 export default function Contact() {
   return (
-    <h2>Contact</h2>
+    <section className='container'>
+      <h2>Contact</h2>
+      <div className="content">
+        Duis minim ut mollit Lorem nisi voluptate velit. Eu tempor non tempor eiusmod non magna aute non qui id ex labore tempor. Ad dolore esse deserunt esse eu labore minim. Elit aute pariatur consequat eu occaecat ea voluptate exercitation non. Dolor magna qui eu commodo anim labore cillum adipisicing Lorem nisi ipsum ad.
+      </div>
+    </section>
   )
 }
