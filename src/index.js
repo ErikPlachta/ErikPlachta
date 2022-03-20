768ea60d38e9efb8c6d99c78df49c648de716e66
Erik Plachta <eplachta@gmail.com>
2022-03-20 11:27:03 -0400

    Onboard uuid and verify integirty

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
+//-- React Specific
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './assets/css/styles.css';
-import './assets/js/script.js';
 import App from './App';
 import reportWebVitals from './utils/reportWebVitals';
 
+//-- 
+import { v4 as uuidv4 } from 'uuid';
+
+//-- Mine
+import './assets/css/styles.css';
+import './assets/js/script.js';
+
 ReactDOM.render(
+  
   <React.StrictMode>
     <App />
   </React.StrictMode>,
