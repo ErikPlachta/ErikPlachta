b4b93937fd88d415268030fa47dc3accf4d4edec
Erik Plachta <eplachta@gmail.com>
2022-03-20 20:10:19 -0400

    Add working email from contact form solution.

--- src/components/main/contact/index.js
+++ src/components/main/contact/index.js
@@ -1,15 +1,47 @@
-import React from 'react';
-import '../../../assets/css/toggle.css'
-
-
-const handleSubmit = e => {
-  console.log(e)
-  e.preventDefault();
-
-};
+import React, { useState } from 'react';
+// import '../../../assets/css/toggle.css'
+import { send } from 'emailjs-com';
 
 
 export default function Contact({ uuid }) {
+
+  //-- preventing default
+  const handleSubmit = e => {
+    console.log(e)
+    e.preventDefault();
+  
+  };
+
+  //-- managing sending email
+  const [toSend, setToSend] = useState({
+    from_name: '',
+    to_name: 'Erik Plachta',
+    message: '',
+    reply_to: '',
+  });
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    send(
+      'service_g06t4eg',
+      'template_rg1kx7u',
+      toSend,
+      'gJH1FFdLLZ-ojMYbQ'
+    )
+      .then((response) => {
+        console.log('SUCCESS!', response.status, response.text);
+      })
+      .catch((err) => {
+        console.log('FAILED...', err);
+      });
+  };
+
+  const handleChange = (e) => {
+    setToSend({ ...toSend, [e.target.name]: e.target.value });
+  };
+
+
+
   return (
     <article className='container'>
       
@@ -21,21 +53,35 @@ export default function Contact({ uuid }) {
         <p className="article-content-title">
           Do you want to get in touch?
         </p>
-      </section>
-    
+      </section>    
 
       <section className="article-content-container contact-form">
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={onSubmit}>
 
           <div className='form-section'>
             
             <span className="form-element">
-              <label htmlFor="fullname">Full Name</label>
-              <input name="fullname" id="fullname" type="text" placeholder="What should I call you?" required />
+              <label htmlFor="from_name">Your Name</label>
+              <input
+                name='from_name'
+                id="from_name"
+                type='text'
+                placeholder='from...'
+                value={toSend.from_name}
+                onChange={handleChange}
+              />
             </span>
             <span className="form-element">
               <label htmlFor="email">Email Address</label>
-              <input name="email" id="email" type="email" placeholder="your@email.com" required />
+              <input
+                name="reply_to"
+                id="email"
+                type="email"
+                placeholder='your@email.com'
+                required
+                value={toSend.reply_to}
+                onChange={handleChange}
+              />
             </span>
             <span className="form-element">
               <label htmlFor="phone">Phone Number</label>
@@ -48,23 +94,25 @@ export default function Contact({ uuid }) {
           <span className="form-element">
             <label htmlFor="reason">Subject</label>
             <select id='subject' name='subject' size='3' required>
-              <option value="none" selected disabled hidden>Select an Option</option>
+              <option value="none" defaultValue disabled hidden>Select an Option</option>
               <option value="social">Social</option>
               <option value="work">Work</option>
               <option value="other">Other</option>
             </select>
           </span>
-          {/* TODO: 03/20/2022 #EP || ADD TOGGLE */}
-          {/* <div id="user-form">
-            <div className="checkboxes">
-              <label for="toggle">Toggle</label>
-              <input type="checkbox" id="toggle" className="toggle"/>
-            </div>
-          </div>, */}
+        
 
           <span className="form-element">
             <label htmlFor="message">Message</label>
-            <textarea name="message" type="textarea" rows='10' placeholder="I'm reaching out because..." required></textarea>
+            <textarea
+              name='message'
+              type="textarea"
+              rows='10'
+              placeholder="I'm reaching out because..."
+              required
+              value={toSend.message}
+              onChange={handleChange}
+              ></textarea>
           </span>
 
           <span className="form-element"> 
@@ -72,6 +120,18 @@ export default function Contact({ uuid }) {
           </span>
         </form>
       </section>
+
+
   </article>
   )
 }
+
+
+
+{/* TODO: 03/20/2022 #EP || ADD TOGGLE */}
+          {/* <div id="user-form">
+            <div className="checkboxes">
+              <label for="toggle">Toggle</label>
+              <input type="checkbox" id="toggle" className="toggle"/>
+            </div>
+          </div>, */}
