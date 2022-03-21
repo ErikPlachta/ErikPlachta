7d36c64d22ac5614c2f95256d08da4ce5d60b42e
Erik Plachta <eplachta@gmail.com>
2022-03-20 23:32:24 -0400

    Add portfolio framework and template content

--- src/components/main/portfolio/index.js
+++ src/components/main/portfolio/index.js
@@ -5,35 +5,69 @@ export default function Portfolio({ uuid }) {
 
   const [portfolioProjects, setPortfolioProjects ] = useState([
     {
+      title: "title 2",
+      description: "desc 2",
+      url: "url 2",
+      img: "img 2",
+      style: .5
+    },
+    {
       title: "title 1",
       description: "desc 1",
       url: "url 1",
-      img: "img 1"
+      img: "img 1",
+      style: .5
+    },
+    {
+      title: "title 1",
+      description: "desc 1",
+      url: "url 1",
+      img: "img 1",
+      style: 1
     },
     {
       title: "title 2",
       description: "desc 2",
       url: "url 2",
-      img: "img 2"
+      img: "img 2",
+      style: .5
+    },
+    {
+      title: "title 1",
+      description: "desc 1",
+      url: "url 1",
+      img: "img 1",
+      style: .5
+    },
+    {
+      title: "title 2",
+      description: "desc 2",
+      url: "url 2",
+      img: "img 2",
+      style: 1
     }
   ])
 
 
   return (
-    <article className='container'>
+    <article className='container' id='portfolio-article'>
       <section>
-        <h2 className="article-header">Portfolio</h2>
+        <h2 className="article-header" id="prtfolio-header">Portfolio</h2>
       </section>
-      <section className="article-content-container">
+      <section className="article-content-container" id='portfolio-container'>
         { portfolioProjects.map( project => (
           // {number}
-          <div className='portfolio-project'>
+          <div
+          // className=`portfolio-project ${flex_100}`
+          // >
+            className={`portfolio-project ${( project.style === 1 && 'flex_100' ) || 'flex_50'}`}
+          >
             <h3>{project.title}</h3>
             <p>{project.description}</p>
             <span>{project.url}</span>
             <img src={project.img} alt={project.title}></img>
           </div>
-        ))};
+        ))}
       </section>
     </article>
   )
