d43e85d6bd16df98571702059a555d9102aa224b
Erik Plachta <eplachta@gmail.com>
2022-03-20 22:43:53 -0400

    Add fully functional social links

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -3,10 +3,24 @@ import React from 'react'
 export default function Footer({ uuid }) {
   return (
     <footer>
-      <span className="nav">GitHub</span>
-      <span className="nav">Linked-In</span>
-      <span className="nav">Twitter</span>
-      <span className="nav">Medium</span>
+
+      <section className='footer-copyright'>
+        Erik Plachta - 2021 ©
+      </section>
+      <section className='footer-social'>
+        <span className="nav">
+          <a href="https://www.github.com/erikplachta" target="_blank" rel="noreferrer" >GitHub</a>
+        </span>
+        <span className="nav">
+          <a href="https://www.linkedin.com/e/erikplachta" target="_blank" rel="noreferrer" >Linked-In</a>
+        </span>
+        <span className="nav">
+          <a href="https://www.twitter.com/erikplachta" target="_blank" rel="noreferrer" >Twitter</a>
+        </span>
+        <span className="nav">
+          <a href="https://blog.erikplachta.com/" target="_blank" rel="noreferrer" >Medium</a>
+        </span>
+        </section>
     </footer>
   )
 }
