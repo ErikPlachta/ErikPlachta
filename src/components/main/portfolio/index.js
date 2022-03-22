ec508580d688efa702f2950f88947e8c86b053c4
Erik Plachta <eplachta@gmail.com>
2022-03-21 20:13:19 -0400

    Add base content for a proejct and verified integrity

--- src/components/main/portfolio/index.js
+++ src/components/main/portfolio/index.js
@@ -1,7 +1,7 @@
 import { object } from 'prop-types'
 import React, { useState } from 'react'
 
-export default function Portfolio({ uuid }) {
+export default function Portfolio({ uuidv4 }) {
 
   const [portfolioProjects, setPortfolioProjects ] = useState([
     {
@@ -9,21 +9,21 @@ export default function Portfolio({ uuid }) {
       description: "desc 2",
       url: "url 2",
       img: "img 2",
-      style: .5
-    },
-    {
-      title: "title 1",
-      description: "desc 1",
-      url: "url 1",
-      img: "img 1",
-      style: .5
-    },
-    {
-      title: "title 1",
-      description: "desc 1",
-      url: "url 1",
-      img: "img 1",
       style: 1
+    },
+    {
+      title: "My Budget - PWA - Concept Application",
+      description: "A Budget concept PWA demonstrating a seamless user experience no matter the users network connection. Hosted on Heroku, running a Node/express.js server, no SQL Database with MongoDB.",
+      url: "https://github.com/ErikPlachta/pwa-personal-budget",
+      img: "https://raw.githubusercontent.com/ErikPlachta/pwa-personal-budget/main/demo.gif",
+      style: 1
+    },
+    {
+      title: "title 1",
+      description: "desc 1",
+      url: "url 1",
+      img: "img 1",
+      style: .5
     },
     {
       title: "title 2",
@@ -63,13 +63,12 @@ export default function Portfolio({ uuid }) {
         { portfolioProjects.map( project => (
           // {number}
           <div
-          // className=`portfolio-project ${flex_100}`
-          // >
+            key={uuidv4()}
             className={`portfolio-project ${( project.style === 1 && 'flex_100' ) || 'flex_50'}`}
           >
             <h3>{project.title}</h3>
             <p>{project.description}</p>
-            <span>{project.url}</span>
+            <span><a href={project.url} target="_blank" ></a></span>
             <img src={project.img} alt={project.title}></img>
           </div>
         ))}
