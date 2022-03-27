5ca5f90caa1f9cccbc494725bb702771cb09ddfd
Erik Plachta <eplachta@gmail.com>
2022-03-27 13:04:30 -0400

    Add update to padding in about. Add finishing content in about.

--- src/components/main/about-me/index.js
+++ src/components/main/about-me/index.js
@@ -3,53 +3,44 @@ import React from 'react';
 export default function AboutMe({ uuidv4 }) {
   return (
     <article className='container'>
-      
+      {/* header banner */}
       <section>
         <h2 className="article-header">Hi, I'm Erik</h2>
-      </section>
-      
-      {/* <section> */}
+      </section>      
+      {/* content */}
       <section className="article-content-container">
-
-        {/* <div className="article-content"> */}
-          
-          {/* <p className="article-content-title">
-            Thanks for checking out my website.
-          </p> 
-          <p className="article-content-indent">
-            I've built this page as a portfolio, 
-          </p> */}
-
-          {/* <p className="article-content-title">
-            IT Engineer, MSP Director, Project Manager, and Software Developer. 
-          </p>
-          <p className="article-content-indent">
-            Officia sunt cillum dolor proident excepteur elit.
-          </p>
-        </div>
-
         <div className="article-content">
-          <p className="article-content-title">
-            Outside of work, my primary focus is family and enjoying life. 
-          </p>
+          <h3 className="article-content-title">
+            A little bit about me...
+          </h3>
           <p className="article-content-indent">
-            Time goes by fast and I want to make memories with the people I love.
+            Professionally, I'm an IT Engineer, MSP Director, Project Manager,
+            and Software Developer. I've spent over 15 years in customer service
+            industry.
+            <br />
+            <br />
+            On a personal level, family and enjoying life is my top priority. 
+            I also spend a lot of time working on software projects, learning
+            new skills, playing music with family, and experiencing new things.
           </p>
-        </div> */}
 
-        <div className="article-content">
-          <p className="article-content-title">
-            Welcome to my professional portfolio.
-          </p>
+          <h3 className="article-content-title">
+            Want to learn more about me or my work?
+          </h3>
           <p className="article-content-indent">
-            Check out the <b>Projects</b> tab to learn about what projects I'm working
-            on. Check out my <b>Resume</b> to learn about my professional experience.
-            If you want to get in touch, head on over to the <b>Contact</b> page or
+            Check out the <b>Projects</b> tab to learn more about my work!
+            <br />
+            <br />
+            Head over to the <b>Resume</b> page to request a copy.
+            <br />
+            <br />
+            If y ou want to get in touch, head on over to the <b>Contact</b> page or
             reach out to me on my social links below.
+            
           </p>
           <p className="signature">- Erik Plachta - March 2022</p>
         </div>
       </section>
     </article>
   )
-}
+};
