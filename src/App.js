687b709501ca9ac4dd2235a336e256ba208100b6
Erik Plachta <eplachta@gmail.com>
2022-04-30 10:58:28 -0400

    Add updating Routing with Links to Nav. Remove Main Component and move About, Resume, Projects, and Contact into pages

--- src/App.js
+++ src/App.js
@@ -3,46 +3,62 @@ import React, { useState } from 'react';
 import {  BrowserRouter, Route, Routes, useParams  } from 'react-router-dom';
 
 
-//-- Header component that contains nav sub-component
+//------------------------------------------------------------------------------
+//-- COMPONENTS
+
 import Header from './components/header';
-
-//-- Main component with sub-components ( Importing subs here vs in main to allow for just 1 locations state obj )
-import Main   from './components/main';
-import AboutMe from './components/main/about-me';
-import Projects from './components/main/projects';
-import Resume from './components/main/resume';
-import Contact from './components/main/contact';
-
-//-- Footer component
 import Footer from './components/footer';
 
+//------------------------------------------------------------------------------
+//-- PAGES
+import About from './pages/About';
+import Resume from './pages/Resume';
+import Projects from './pages/Projects';
+import Contact from './pages/Contact';
 
+
+//------------------------------------------------------------------------------
+//-- App Function
 export default function App({ uuidv4 }) {
-  // <script src="https://www.google.com/recaptcha/api.js" async defer></script>
-     
+    
+  //----------------------------------------------------------------------------
+  //-- STATES
   
-
-  //-- Sub-Components within the main container and also used for navigation
+  //-- Components within the main element. State is managed by Navigation and Routing.
   const [locations, setLocations] = useState({
-    'about':     { name:'about',      subComponent:{AboutMe},    key:uuidv4(), jsx: <AboutMe   uuidv4={uuidv4}></AboutMe>   },
-    'resume':    { name:'resume',     subComponent:{Resume},     key:uuidv4(), jsx: <Resume    uuidv4={uuidv4}></Resume>    },
-    'projects': { name:'projects',  subComponent:{Projects},  key:uuidv4(), jsx: <Projects uuidv4={uuidv4}></Projects> },
-    'contact':   { name:'contact',    subComponent:{Contact},    key:uuidv4(), jsx: <Contact   uuidv4={uuidv4}></Contact>   },
+    'about':    { name:'about',     subComponent:{About},    key:uuidv4(), jsx: <About    uuidv4={uuidv4}>  </About>    },
+    'resume':   { name:'resume',    subComponent:{Resume},   key:uuidv4(), jsx: <Resume   uuidv4={uuidv4}>  </Resume>   },
+    'projects': { name:'projects',  subComponent:{Projects}, key:uuidv4(), jsx: <Projects uuidv4={uuidv4}>  </Projects> },
+    'contact':  { name:'contact',   subComponent:{Contact},  key:uuidv4(), jsx: <Contact  uuidv4={uuidv4}>  </Contact>  },
   });
 
-  //-- setting default location to about me
+  //-- setting default location to About. Used by Nav for location awareness
   const [ location, setLocation ] = useState( 'about' );
     
   //-- Returning all content
   return (
-    <section>
+    <>
       <BrowserRouter>
-        <Header key={uuidv4()} uuidv4={uuidv4} locations={locations} setLocations={setLocations} location={location} setLocation={setLocation}></Header>
-        <Routes>
-          <Route exact path="/" element={<Main key={uuidv4()} uuidv4={uuidv4} locations={locations} setLocations={setLocations} location={location} setLocation={setLocation} />} />
-        </Routes>
+        <Header
+          key={uuidv4()}
+          uuidv4={uuidv4}
+          locations={locations}
+          setLocations={setLocations}
+          location={location}
+          setLocation={setLocation}
+        ></Header>
+        <main>
+          <Routes>
+          <Route exact path="/" element={locations['about'].jsx}/>
+            <Route exact path="/about" element={locations['about'].jsx}/>
+            <Route exact path="/resume" element={locations['resume'].jsx}/>
+            <Route exact path="/projects" element={locations['projects'].jsx}/>
+            <Route exact path="/contact" element={locations['contact'].jsx}/>            
+            <Route exact path="/*" element={locations['about'].jsx}/> {/* All typos or non-existing paths to go to default page */}
+          </Routes>
+        </main>
         <Footer key={uuidv4()} uuidv4={uuidv4}></Footer>
       </BrowserRouter>
-    </section>
+    </>
   );
 };
--- src/components/header/navigation/index.js
+++ src/components/header/navigation/index.js
@@ -1,4 +1,5 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import { capitalizeFirstLetter } from '../../../utils/helpers';
 
 //-- Being used to generate unique keys for react
@@ -13,15 +14,24 @@ export default function Navigation({ location, setLocation, locations, setLocati
   return (
     <nav>
       {Object.keys(locations).map( (name, number) => (
+        
         <span
           //-- if clicked event to update the location state to update the DOM
-          onClick={() => { setLocation(locations[name].name) }}
+          onClick={() => { 
+            setLocation(locations[name].name)
+            console.log("test")
+          }}
           //-- if location name is current location, add className nav-active. otherwise nothing
           className={`${( location === locations[name].name && 'nav nav-active' ) || 'nav'}`}
           key={`${locations[name].key}`}
         >
-          {capitalizeFirstLetter(locations[name].name)}
+
+          {/* Create Link to route of location around string */}
+          <Link to={"/" + locations[name].name}>
+            {capitalizeFirstLetter(locations[name].name)}
+          </Link>
         </span>
+        
       ))}
     </nav>
   )
