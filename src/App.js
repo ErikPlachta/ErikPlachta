1c0d0b4ca3e35226a6315a5ab76db2cfbf96a627
Erik Plachta <eplachta@gmail.com>
2022-04-30 11:49:44 -0400

    Add Nav route requested awareness to update Nav component accordingly.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-//-- Layer 2: ROOT ELEMENT MANAGING CONTENT AND UI
+//------------------------------------------------------------------------------
+//-- IMPORTS
 import React, { useState } from 'react';
-import {  BrowserRouter, Route, Routes, useParams  } from 'react-router-dom';
-
+import {  BrowserRouter, Route, Routes, useParams } from 'react-router-dom';
 
 //------------------------------------------------------------------------------
 //-- COMPONENTS
-
 import Header from './components/header';
 import Footer from './components/footer';
 
@@ -24,18 +23,17 @@ export default function App({ uuidv4 }) {
   //----------------------------------------------------------------------------
   //-- STATES
   
-  //-- Components within the main element. State is managed by Navigation and Routing.
-  const [locations, setLocations] = useState({
+  const [locations, setLocations] = useState({ //-- Components within the main element. State is managed by Navigation and Routing.
     'about':    { name:'about',     subComponent:{About},    key:uuidv4(), jsx: <About    uuidv4={uuidv4}>  </About>    },
     'resume':   { name:'resume',    subComponent:{Resume},   key:uuidv4(), jsx: <Resume   uuidv4={uuidv4}>  </Resume>   },
     'projects': { name:'projects',  subComponent:{Projects}, key:uuidv4(), jsx: <Projects uuidv4={uuidv4}>  </Projects> },
     'contact':  { name:'contact',   subComponent:{Contact},  key:uuidv4(), jsx: <Contact  uuidv4={uuidv4}>  </Contact>  },
   });
 
-  //-- setting default location to About. Used by Nav for location awareness
-  const [ location, setLocation ] = useState( 'about' );
-    
-  //-- Returning all content
+  const [ location, setLocation ] = useState( 'about' ); //-- setting default location to About. Used by Nav for location awareness
+
+  //----------------------------------------------------------------------------
+  //-- Return Function
   return (
     <>
       <BrowserRouter>
--- src/components/header/navigation/index.js
+++ src/components/header/navigation/index.js
@@ -1,37 +1,53 @@
-import React, { useEffect } from 'react';
+//------------------------------------------------------------------------------
+//-- IMPORTS
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { useLocation } from "react-router-dom";
 import { capitalizeFirstLetter } from '../../../utils/helpers';
 
-//-- Being used to generate unique keys for react
-// import { v4 as uuidv4 } from 'uuid';
+//------------------------------------------------------------------------------
+//-- App Function
+export default function Navigation({ location, setLocation, locations, setLocations }) {  
 
-
-export default function Navigation({ location, setLocation, locations, setLocations }) {
+  //-- Extracts the current location requested in URL
+  const [currentLocation, setCurrentLocation] = useState(
+    (useLocation().pathname).split("/")[1]
+  );
   
-  //-- Update DOM title
-  useEffect(() => { document.title = `${capitalizeFirstLetter(location)} - Erik Plachta`; }, [location]);
+  //----------------------------------------------------------------------------
+  //-- Update DOM title and set location based on requested Route
+  useEffect(() => {
+      document.title = `${capitalizeFirstLetter(location)} - Erik Plachta`;
+      
+      //-- if the requested Route is an existing location, update nav accordingly
+      if(locations[currentLocation]){
+        setLocation(currentLocation)
+      }
+      //-- Otherwise set to default location for nav awareness
+      else {
+        setLocation(location);
+        
+      }
+    },
+    []
+  );
 
+  //----------------------------------------------------------------------------
+  //-- Return Function
   return (
     <nav>
-      {Object.keys(locations).map( (name, number) => (
-        
+      {/* {setLocation(useCurrentPath())} */}
+      {/* Build Navigation based on locations state object values */}
+      {Object.keys(locations).map( (name, number) => ( 
         <span
-          //-- if clicked event to update the location state to update the DOM
-          onClick={() => { 
-            setLocation(locations[name].name)
-            console.log("test")
-          }}
-          //-- if location name is current location, add className nav-active. otherwise nothing
-          className={`${( location === locations[name].name && 'nav nav-active' ) || 'nav'}`}
+          onClick={() => {  setLocation(locations[name].name) }} //-- if clicked event to update the location state to new location
+          className={`${( location === locations[name].name && 'nav nav-active' ) || 'nav'}`} //-- if location name is current location, add className nav-active. otherwise nothing
           key={`${locations[name].key}`}
         >
-
-          {/* Create Link to route of location around string */}
-          <Link to={"/" + locations[name].name}>
+          <Link to={"/" + locations[name].name}>  {/* If link selected, re-route to proper location */}
             {capitalizeFirstLetter(locations[name].name)}
           </Link>
         </span>
-        
       ))}
     </nav>
   )
