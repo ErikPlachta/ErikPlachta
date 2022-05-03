64eeafff7f36c5542a67e4366ee5b6283f3aac3b
Erik Plachta <eplachta@gmail.com>
2022-05-03 19:14:53 -0400

    Move resume post projects

--- src/App.js
+++ src/App.js
@@ -27,8 +27,8 @@ export default function App({ uuidv4 }) {
   
   const [locations, setLocations] = useState({ //-- Components within the main element. State is managed by Navigation and Routing.
     'about':    { name:'about',     subComponent:{About},    key:uuidv4(), jsx: <About    uuidv4={uuidv4} location={location} setLocation={setLocation} >  </About>    },
-    'resume':   { name:'resume',    subComponent:{Resume},   key:uuidv4(), jsx: <Resume   uuidv4={uuidv4} >  </Resume>   },
     'projects': { name:'projects',  subComponent:{Projects}, key:uuidv4(), jsx: <Projects uuidv4={uuidv4} >  </Projects> },
+    'resume':   { name:'resume',    subComponent:{Resume},   key:uuidv4(), jsx: <Resume   uuidv4={uuidv4} >  </Resume>   },
     'contact':  { name:'contact',   subComponent:{Contact},  key:uuidv4(), jsx: <Contact  uuidv4={uuidv4} >  </Contact>  },
   });
 
