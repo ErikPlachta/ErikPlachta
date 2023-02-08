d93395151f2dd3d034c6cdd5905f62bb5a824087
Erik Plachta <eplachta@gmail.com>
2023-02-07 19:59:38 -0500

    Removed ALL un-needed content.

--- _Share/_document.tsx
+++ _Share/_document.tsx
@@ -1,84 +1,23 @@
-import React, { createContext, useEffect }  from 'react';
 import { Html, Head, Main, NextScript } from 'next/document';
-import Footer from '@/components/footer';
-
-import Interfaces from "@/interfaces/index";  //TODO: 20230205 #EP || Build ths out or delete it.
-import NavItems from "@/components/context/Navigation";
-export const GlobalContext = createContext({});
-
 
 /** Second level in Next.js document tree, where my top-level components are rendered.
- * 
+ *
+ * Note: This seems to render AFTER the page and only on hard page refresh, not internal navigation.
  */
-export default function Document( props: any ) {
-
-  const GlobalContextValue = {
-    interfaces  : Interfaces,                    //TODO: 20230205 #EP || Build ths out or delete it.
-    Navigation  : NavItems,
-  };
-
-  const getPathData = () => {
-    const path = props?.__NEXT_DATA__?.page.split("/");
-    const numberPath = path?.length;
-    console.log("path: ", path)
-    
-    return {
-      current : path[numberPath - 1],
-      previous : path.slice(0, numberPath - 1)
-    }
-  }
-  // console.log(setPathData())
-
-  // TODO: Verify if this is the right way to force a re-render when props change or if should send props somewhere else instead.
-  useEffect(() => { }, [props]);
-
+export default function Document() {
   return (
-    <GlobalContext.Provider value={GlobalContextValue}>
       <Html className='h-full' lang="en">
         <Head/>
         <body
           className={
               `h-full `
-              // + `bg-gradient-to-b from-slate-900 to-slate-900/90 `
             + `bg-blue-900 `
             + `background-animate bg-gradient-radial from-blue-800 via-blue-900 to-blue-800`
         }
         >
           <Main />
           <NextScript />
-          <Footer 
-            developerName='Erik Plachta'
-            showDeveloperName={true}
-            brandName={'ErikPlachta.com'}
-            showBrandName={false}
-            logo={{
-              image: [],
-              alt: 'Erik Plachta',
-              link: {
-                href: '/',
-                target: '_self',
-                rel: 'home',
-                name: 'Erik Plachta',
-              }
-            }}
-            
-            BreadCrumb={{
-              ...getPathData()
-            }}
-          />
         </body>
       </Html>
-    </GlobalContext.Provider>
   )
 };
-
-
-
-export function getServerSideProps() {
-  console.log("getServerSideProps: " , getServerSideProps)
-  return {
-    props: {
-      // TODO: 20230206 || Add additional props.
-    },
-  }
-}
